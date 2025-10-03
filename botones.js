b2d64926a574239dd98ba51346fde19ba83bed3a
candelagarbarinoo <c.garbarinorazzeto@colegiocalasanz.edu.ar>
2025-10-03 15:18:35 -0300

    Add files via upload

---
+++ botones.js
@@ -0,0 +1,18 @@
+// Botón 1: Modo Claro
+document.getElementById('btn1').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  });
+  
+  // Botón 2: Modo Oscuro
+  document.getElementById('btn2').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#222';
+    document.body.style.color = 'white';
+  });
+  
+  // Botón 3: Modo Creativo
+  document.getElementById('btn3').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'hotpink';
+    document.body.style.color = 'navy';
+  });
+  
